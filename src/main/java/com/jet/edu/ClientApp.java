9e0bc0c964cbbde335e44b582c00b7f8824f4d27
Yuriy <yura@zhayvoronok.com>
2015-11-12 12:16:27 +0300

    Server & Client App

--- src/main/java/com/jet/edu/ClientApp.java
+++ src/main/java/com/jet/edu/ClientApp.java
@@ -3,8 +3,8 @@ package com.jet.edu;
 /**
  * A-Team project
  */
-public class App {
+public class ClientApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Client started!");
     }
 }
---
+++ src/main/java/com/jet/edu/ServerApp.java
@@ -0,0 +1,10 @@
+package com.jet.edu;
+
+/**
+ * A-Team project
+ */
+public class ServerApp {
+    public static void main(String[] args) {
+        System.out.println("Server started!");
+    }
+}
