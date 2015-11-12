d49b5c9b59c09e7c77f3bacdd79b0edde521be20
Rylkin Maksim <msrylkin@gmail.com>
2015-11-12 16:48:11 +0300

    test

---
+++ src/main/java/com/jet/edu/client/Client.java
@@ -0,0 +1,7 @@
+package com.jet.edu.client;
+
+/**
+ * Created by user on 12.11.2015.
+ */
+public interface Client {
+}
--- src/test/java/com/acme/edu/TestChatStorage.java
+++ src/test/java/com/acme/edu/TestChatStorage.java
@@ -3,12 +3,9 @@ package com.acme.edu;
 
 import com.jet.edu.server.ChatStorage;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  * Created by user on 12.11.2015.
@@ -27,7 +24,6 @@ public class TestChatStorage {
 
     }
 
-
     @Test
     public void ShouldConnectToDb() throws Exception{
         chatStorage.connect();
@@ -35,11 +31,34 @@ public class TestChatStorage {
 
     @Test
     public void shouldDisconnectFromDB() throws Exception{
-        chatStorage.disconnet();
+        chatStorage.disconnect();
     }
 
     @Test
     public void shouldCorrectlyAddUsers() throws Exception{
+        chatStorage.connect();
         chatStorage.addUser("USER1");
     }
+
+    @Test
+    public void shouldCorrectlyReturnOnlineStatus() throws Exception{
+        chatStorage.connect();
+
+        Assert.assertFalse(chatStorage.isUserOnline("Yura"));   }
+
+    @Test
+    public void shouldSetOffline() throws Exception{
+        chatStorage.connect();
+        chatStorage.setUserOffline("Yura");
+        Assert.assertFalse(chatStorage.isUserOnline("Yura"));
+    }
+
+
+    @Test
+    public void addMessage() throws Exception{
+        chatStorage.connect();
+        chatStorage.addMessage("Yura","Hello!");
+    }
+
+
 }
