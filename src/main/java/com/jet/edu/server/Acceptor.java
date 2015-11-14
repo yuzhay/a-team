90c61e1596925fd7237bd9060a8b3c9d2ac4b876
Yuriy <yura@zhayvoronok.com>
2015-11-14 07:31:57 +0300

    Refactoring

--- src/main/java/com/jet/edu/server/Acceptor.java
+++ src/main/java/com/jet/edu/server/Acceptor.java
@@ -2,7 +2,12 @@ package com.jet.edu.server;
 
 import com.jet.edu.ChatLogger;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
