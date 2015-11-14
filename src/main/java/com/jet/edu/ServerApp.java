8f38994e8a0646803656f8fc325ad860408acda1
Yuriy <yura@zhayvoronok.com>
2015-11-14 08:55:31 +0300

    Input/Output client

--- src/main/java/com/jet/edu/ServerApp.java
+++ src/main/java/com/jet/edu/ServerApp.java
@@ -1,6 +1,7 @@
 package com.jet.edu;
 
 import com.jet.edu.server.ChatServer;
+import java.io.UnsupportedEncodingException;
 
 /**
  * * Project04
@@ -8,8 +9,10 @@ import com.jet.edu.server.ChatServer;
 public class ServerApp {
     private ServerApp(){}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         int port = 12348;
+
+        System.setOut(new java.io.PrintStream(System.out, true, "Cp866"));
         ChatServer server = new ChatServer(port);
         server.start();
         System.out.println("Chat Server started on localhost:" + port +" successful");
